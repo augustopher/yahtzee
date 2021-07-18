02093a2a4139766a5118b422865818d35f9edd54
Gus Powers <guspowers0@gmail.com>
2021-07-18 19:18:50 -0400

    make local dev script more flexible

--- run_checks.sh
+++ run_checks.sh
@@ -1,22 +1,53 @@
 #!/bin/bash
 
-echo "Lint check with flake8..."
-flake8 yahtzee tests
-# flake8 doesn't provide feedback when no issues are found
-if [ $? -eq 0 ]; then
-	echo "flake8 found no issues."
-else
-	exit 1
+OPTIND=1
+
+RUN_LINT=0
+RUN_TYPE=0
+RUN_TEST=0
+
+while getopts "almt" opt; do
+  case "$opt" in
+    a)
+      RUN_LINT=1
+      RUN_TYPE=1
+      RUN_TEST=1
+      ;;
+    l)
+      RUN_LINT=1
+      ;;
+    m)
+      RUN_TYPE=1
+      ;;
+    t)
+      RUN_TEST=1
+      ;;
+  esac
+done
+
+if [ $RUN_LINT -gt 0 ]; then
+	echo "Lint check with flake8..."
+	flake8 yahtzee tests
+	# flake8 doesn't provide feedback when no issues are found
+	if [ $? -eq 0 ]; then
+		echo "flake8 found no issues."
+	else
+		exit 1
+	fi
 fi
 
-echo "Type check with mypy..."
-mypy yahtzee
-if [ $? -ne 0 ]; then
-	exit 1
+if [ $RUN_TYPE -gt 0 ]; then
+	echo "Type check with mypy..."
+	mypy yahtzee
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
 fi
 
-echo "Run tests with pytest..."
-pytest tests
-if [ $? -ne 0 ]; then
-	exit 1
+if [ $RUN_TEST -gt 0 ]; then
+	echo "Run tests with pytest..."
+	pytest tests
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
 fi
